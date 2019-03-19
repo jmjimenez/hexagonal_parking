212ea040a1670740493a4172cf7edb1b37e19baf
Jose Manuel Jimenez <jose.jimenezgarcia@voiceworks.com>
2019-03-19 10:44:34 +0100

    New test DeleteParkingSlotTest

---
+++ web/app/test/unit/Application/Command/Handler/InMemory/Pdo/DeleteParkingSlotTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Jmj\Test\Unit\Application\Command\Handler\InMemory\Pdo;
+
+use Jmj\Parking\Application\Command\DeleteParkingSlot as DeleteParkingSlotPayload;
+use Jmj\Parking\Application\Command\Handler\DeleteParkingSlot;
+use Jmj\Parking\Application\Command\Handler\Exception\ParkingNotFound;
+use Jmj\Parking\Application\Command\Handler\Exception\UserNotFound;
+use Jmj\Parking\Common\Exception\PdoConnectionError;
+use Jmj\Parking\Common\Exception\PdoExecuteError;
+use Jmj\Parking\Common\NormalizeDate;
+use Jmj\Parking\Common\DomainEventsRegister;
+use Jmj\Parking\Domain\Aggregate\Parking;
+use Jmj\Parking\Domain\Aggregate\ParkingSlot;
+use Jmj\Parking\Domain\Exception\ExceptionGeneratingUuid;
+use Jmj\Parking\Domain\Exception\ParkingException;
+use PHPUnit\Framework\TestCase;
+
+class DeleteParkingSlotTest extends TestCase
+{
+    use DomainEventsRegister;
+    use DataSamplesGenerator;
+    use NormalizeDate;
+    use AssertSqlStatements;
+
+    /**
+     * @throws ParkingNotFound
+     * @throws UserNotFound
+     * @throws PdoConnectionError
+     * @throws PdoExecuteError
+     * @throws ExceptionGeneratingUuid
+     * @throws ParkingException
+     * @throws \Exception
+     */
+    public function testExecute()
+    {
+        $this->createTestCase();
+
+        $this->configureDomainEventsBroker();
+
+        $this->startRecordingEvents();
+
+        $payload = new DeleteParkingSlotPayload(
+            $this->userAdmin->uuid(),
+            $this->parking->uuid(),
+            $this->parkingSlotOne->uuid()
+        );
+
+        $command = new DeleteParkingSlot(
+            $this->userRepository,
+            $this->parkingRepository
+        );
+        $command->execute($payload);
+
+        $this->assertEquals(
+            [ ParkingSlot::EVENT_PARKING_SLOT_DELETED, Parking::EVENT_PARKING_SLOT_DELETED_FROM_PARKING ],
+            $this->recordedEventNames
+        );
+
+        $parkingFound = $this->parkingRepository->findByUuid($this->parking->uuid());
+        $parkingSlotFound = $parkingFound->getParkingSlotByUuid($this->parkingSlotOne->uuid());
+        $this->assertNull($parkingSlotFound);
+
+        $this->assertEquals(1, count($this->recordedSqlStatements));
+        $this->assertUpdate($this->recordedSqlStatements[0], 'Parking', ['uuid' => $this->parking->uuid()]);
+    }
+}
